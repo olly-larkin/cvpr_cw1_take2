521c15483685a3e029c0d1e2e369114de545f367
NGovani <name@govani.org>
2021-02-17 15:31:04 +0000

    updated report

--- Task2/plotPoints.m
+++ Task2/plotPoints.m
@@ -1,9 +1,8 @@
-load('img1&2_points.mat')
 figure(1)
-imshow(imread('PNGs/DSC_0017.png'))
+imshow(imread('../ImagesNew/HG/IMG_3056.JPG'))
 hold on;
 plot(x1, y1, 'ro', 'LineWidth', 2, 'MarkerSize', 5); 
 figure(2)
-imshow(imread('PNGs/DSC_0019.png'))
+imshow(imread('../ImagesNew/HG/IMG_3058.JPG'))
 hold on;
 plot(x2, y2, 'ro', 'LineWidth', 2, 'MarkerSize', 5); 
